4cf358920485b4bd07bed8a930959966391a05a7
David <dchapa999@gmail.com>
2020-09-18 23:39:38 -0500

    fixed root route

--- bookcollection/config/routes.rb
+++ bookcollection/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'bookcollection#index'
+  root 'books#index'
 
   resources :books do
     member do
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'bookcollection/index'
+  get 'books#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
